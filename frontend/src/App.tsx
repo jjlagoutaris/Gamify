5d6144f56c44add85b4db8aacb133ebd757530f8
John Lagoutaris <jjlagoutaris@gmail.com>
2023-03-07 23:02:39 -0500

    Add spinning cookie

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import { images } from './constants';
 
 function App() {
   return (
     <div className="App">
+      <img src={images.cookie} className="App-logo" alt="cookie" />
     </div>
   );
 }
---
+++ frontend/src/constants/images.js
@@ -0,0 +1,8 @@
+import cookie from '../assets/cookie.png';
+import bittenCookie from '../assets/cookieBitten.png';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default{
+  cookie,
+  bittenCookie,
+}
