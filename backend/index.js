e8af89ea56d7f03c339bdfb47ec3e462467ac517
John Lagoutaris <jjlagoutaris@gmail.com>
2023-03-10 17:34:30 -0500

    Set up routing for backend

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,23 @@
-import { Express } from "express";
-import { mongoose } from "mongoose";
-import { BodyParser } from "body-parser";
-import { cors } from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+// connecting to the db
+
+const CONNECTION_URL = `mongodb+srv://jjlagoutaris:lXpSRmqwKDqJg0WC@freecluster.fuuucdh.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL) // connect to mongoDB
+        .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))) // if successful, listen for port
+        .catch((error) => console.log(error.message)); // else, throw error
