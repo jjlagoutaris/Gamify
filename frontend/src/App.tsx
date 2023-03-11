b6a53d9a68f21b10c24952d92fbdbe90f8c95475
John Lagoutaris <jjlagoutaris@gmail.com>
2023-03-11 14:12:09 -0500

    Add Footer & make responsive

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.scss';
 import { images } from './constants';
 import { Container } from 'react-bootstrap';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  return (
-    <Container fluid className='app__hi'>
-      <img src={images.cookie} className="App-logo" alt="cookie" />
-    </Container>
-  );
+  return(
+    <Footer />
+  )
+    // <Container fluid className='app__flexColumn'>
+    //   <img src={images.cookie} className="app__logo" alt="cookie" />
+    // </Container>
 }
 
 export default App;
---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Footer.scss";
+import { Container } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <>
+      <Container className="app__footer app__flexColumn app__normal-text">
+        <p className="app__footer-top">
+        © Copyright John Lagoutaris. All Rights Reserved.
+        </p>
+        <p className="app__footer-bottom">
+          Website by <a href="https://jjlagoutaris.github.io/myWebsite/" target="_blank" rel="noreferrer" className='app__footer-name'>John Lagoutaris</a>
+        </p>
+      </Container>
+    </>
+  )
+}
+
+export default Footer
