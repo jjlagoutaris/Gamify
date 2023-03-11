7a543d45779205ce1d70db311b668578981cf14f
John Lagoutaris <jjlagoutaris@gmail.com>
2023-03-11 07:13:54 -0500

    Add bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.scss';
 import { images } from './constants';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
+    <Container fluid className='app__hi'>
       <img src={images.cookie} className="App-logo" alt="cookie" />
-    </div>
+    </Container>
   );
 }
 
