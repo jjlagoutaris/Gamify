b43cb042827bf04b4eaa6da98e4c605424767e9e
John Lagoutaris <jjlagoutaris@gmail.com>
2023-03-12 14:03:41 -0400

    Header set up and responsive, implemented themeSwitcher

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,13 +1,46 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
-import './Header.scss';
+import React, {useState, useEffect} from "react";
+import { Container } from "react-bootstrap";
+import "./Header.scss";
+import { images } from "../../constants";
 
 const Header = () => {
-  return (
-    <Container className="header-container" fluid>
-      Header
-    </Container>
-  )
-}
 
-export default Header
+  const [theme, setTheme] = useState(false);
+
+  useEffect(() => {
+    if(document.body.classList.contains('dark')){
+      document.body.classList.remove('dark');
+    } else{
+      document.body.classList.add('dark');
+    }
+  });
+
+  return (
+    <Container className="header-container app__flexCenter" fluid>
+      <p></p>
+      <div className="header-logo-container app__flexCenter">
+        <img
+          src={images.toDo}
+          alt="To Do"
+          className="header-image"
+        />
+        <h1 className="app__header-text">Or Not</h1>
+        <img
+          src={images.toDo}
+          alt="To Do"
+          className="header-image"
+        />
+      </div>
+
+      <img
+        alt="Light/Dark Mode Selector"
+        className="header-image-btn header-image"
+        onClick={() => setTheme(prevMode => !prevMode)}
+        src={theme ? images.darkMode : images.lightMode}
+      />
+      
+    </Container>
+  );
+};
+
+export default Header;
--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,10 +1,14 @@
 import cookie from '../assets/cookie.png';
 import bittenCookie from '../assets/cookieBitten.png';
-import activities from '../assets/lifestyles.png';
+import toDo from '../assets/ToDo.png';
+import darkMode from '../assets/darkMode.png';
+import lightMode from '../assets/lightMode.png';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default{
   cookie,
   bittenCookie,
-  activities,
+  toDo,
+  darkMode,
+  lightMode,
 }
