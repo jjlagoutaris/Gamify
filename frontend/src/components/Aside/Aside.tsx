5a73391bf498a8c39c5f5833605379d1367a4370
John Lagoutaris <jjlagoutaris@gmail.com>
2023-03-12 21:50:04 -0400

    Set up Tasks

--- frontend/src/components/Aside/Aside.tsx
+++ frontend/src/components/Aside/Aside.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Button, Container, Row } from 'react-bootstrap';
-import { images } from '../../constants';
+import { Container, Row } from 'react-bootstrap';
 import './Aside.scss';
 
 const Aside = () => {
@@ -17,16 +16,15 @@ const Aside = () => {
       </Row>
       <Row className="aside-projects">        
         <h4 className="app__header-text aside-header">Projects</h4>
-        {/* <ul className="aside-projects-categories">
-          <li className="aside-label">Project</li>
-        </ul> */}
-        <Button className='aside-button aside-label'>
-          Add Project
-        </Button>
-
-      </Row>
-      <Row className='app__flexColumn'>
-
+        <ul className="aside-tasks-categories">
+          <li className="aside-label">All T@sks</li>
+          <li className="aside-label">T0day</li>
+          <li className="aside-label">Next 7 Day$</li>
+          <li className="aside-label">!mportant</li>
+        </ul>
+        <button className='aside-button'>
+          Add Task
+        </button>
       </Row>
     </Container>
   )
--- frontend/src/components/Tasks/Tasks.tsx
+++ frontend/src/components/Tasks/Tasks.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Button, Container, Row } from 'react-bootstrap'
+import './Tasks.scss';
 
 const Tasks = () => {
   return (
-    <Container className="app__container tasks-container app__flexColumn" fluid>
-      Tasks
+    <Container id="tasks-container" className="app__container" fluid>
+      <Row className='tasks-row app__flexColumn app__container'>
+        <h2 className="tasks-header app__header-text">All Tasks</h2>
+        <ul className="tasks-ul">
+          <li className='tasks-label'>Example Task</li>
+        </ul>
+        <button className='tasks-button'>
+          --- Add Task ---
+        </button>
+      </Row>
     </Container>
   )
 }
