2ce8667460c11868332820bbfe292137acd8535a
John Lagoutaris <jjlagoutaris@gmail.com>
2023-03-16 22:05:27 -0400

    Add type checking for env variables Separate app content from db content

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,10 @@
-import "dotenv/config";
+import app from './app';
+import env from "./utilities/validateEnv";
 import mongoose from "mongoose";
-import express from "express";
-const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-const CONNECTION_URL = `mongodb+srv://${process.env.MONGO_URI}@freecluster.fuuucdh.mongodb.net/?retryWrites=true&w=majority`;
-const port = process.env.PORT || 5000;
+const CONNECTION_URL = `mongodb+srv://${env.MONGO_URI}@freecluster.fuuucdh.mongodb.net/?retryWrites=true&w=majority`;
+const port = env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL) // connect to mongoDB
         .then(() => app.listen(port, () => console.log(`Server running on port: ${port}`))) // if successful, listen for port
         .catch((error) => console.log(error.message)); // else, throw error
-
-
