63a275041954c214baf91ef43cdcdb6c195d7724
John Lagoutaris <jjlagoutaris@gmail.com>
2023-03-17 12:37:30 -0400

    Get Task serverside and implement error handling and error forwarding

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,31 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import TaskModel from './models/task';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res, next) => {
+  try {
+    const tasks = await TaskModel.find().exec();
+    res.status(200).json(tasks);
+  } catch (error) {
+    next(error);
+  }
+
+});
+
+// forward to error handler if endpoint is not found
+app.use((req, res, next) => {
+  next(Error("Endpoint not found"));
+});
+
+// error handler middleware
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = "An unknown error occurred";
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,8 @@ import app from './app';
 import env from "./utilities/validateEnv";
 import mongoose from "mongoose";
 
-const CONNECTION_URL = `mongodb+srv://${env.MONGO_URI}@freecluster.fuuucdh.mongodb.net/?retryWrites=true&w=majority`;
+const dbName = `2DoApp`;
+const CONNECTION_URL = `mongodb+srv://${env.MONGO_URI}@freecluster.fuuucdh.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 const port = env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL) // connect to mongoDB
